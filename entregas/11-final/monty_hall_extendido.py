8411885b333c9abf18124c7a7d540c74c6b39c24
lucagaziglia <lucagaziglia@gmail.com>
2025-11-24 22:42:12 -0300

    Corregí justificación del MontyHallExtendido

--- entregas/11-final/monty_hall_extendido.py
+++ entregas/11-final/monty_hall_extendido.py
@@ -109,7 +109,6 @@ def pEpisodio_DatosMa(Episodio, Datos):
 df_monty = pd.read_csv(
     '../../materiales_del_curso/11-final/datos/NoMontyHall.csv')
 Datos = list(df_monty.iloc[:2000].itertuples(index=False, name=None))
-Datos[0]
 # %%
 evidencias = []
 datos_previos = []
@@ -131,20 +130,17 @@ df_evidencia = pd.DataFrame(
 df_evidencia.to_csv('../../entregas/11-final/EvidenciasNoMontyHall.csv')
 
 # %%
-plt.figure(figsize=(12, 5))
 
-plt.plot(df_evidencia['Base'], label="M0 Base")
-plt.plot(df_evidencia['MontyHall'], label="M1 Monty")
-plt.plot(df_evidencia['Alternativo'], label="M2 Alternativo")
-
-plt.xlabel("Episodio")
-plt.ylabel("Evidencia P(c,s,r | M)")
-plt.title("Evidencia por episodio según cada modelo")
-plt.legend()
-plt.yscale("log")  # MUY recomendable: las probabilidades son muy pequeñas
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+for col in df_evidencia.columns:
+    plt.plot(df_evidencia[col], label=f"Modelo: {col}")
+    plt.xlabel("Episodio")
+    plt.ylabel("Evidencia P(c,s,r | M)")
+    plt.title("Evidencia por episodio")
+    plt.legend()
+    plt.yscale("log")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
 
 # %%
 plt.figure(figsize=(12, 4))
@@ -167,6 +163,29 @@ plt.yscale("log")
 plt.tight_layout()
 plt.show()
 
-# %%
+# %% [markdown]
+# # Justificación de los resultados.
+# ##
+# Tuve bastantes complicaciones para traer las funciones de la practica 1 que hicimos al principio de la cursada. Por lo cual, definí todas las funciones necesarias nuevamente para poder calcular las evidencias de los modelos.
+#
+# El codigo para calcular las evidencias y armar el dataframe es bastante facil de entender, si es que se conocen esas funciones que definimos al principio del archivo. Lo que me gustaría comentar es un poco el porque los gráficos son de esa forma.
+#
+# ## Evidencia por episodio (plots de líneas)
+# En este gráfico separe en tres gráficos distintos los 3 modelos con el objetivo de poder entenderlos, ya que se hacia complicado entenderlos cuando se superponían mucho.
+#
+# ### Modelo Base (M0):
+# Este modelo simula la situación en la cual el host elige una caja al azar (siempre que no sea el premio). Donde la evidencia se mantiene constante a lo largo de todos los episodios, alrededor del valor de 0,055.
+#
+# $$ P(c,s,r \mid M_0) = \underbrace{\frac{1}{3}}_{\text{regalo}} \times \underbrace{\frac{1}{3}}_{\text{elección}} \times \underbrace{\frac{1}{2}}_{\text{abrir}} = \frac{1}{18} \approx 0.055 $$
 
-'''Justificación del ejercicio'''
+# ### Modelo Monty Hall (M1):
+# Cuando el host sigue las reglas, este modelo predice MUCHO mejor que el base (el doble de probabilidad, $\approx 0.11$). El bloque denso pareciendo que el modelo ocupa distintas evidencias es una confusión visual por lo mucho que se sorprende el modelo en cuanto el presentador 'se olvida' y muestra la misma caja que recibimos. Esto sucede, ya que el modelo le asigna probabilidad 0 a ese evento, y en este gráfico (al estar en escala logaritmica), se va a menos infinito.
+
+# ### Modelo Alternativo (M2):
+# Este modelo es el que mejor predice los datos, ya que llega a los mismos valores que el modelo Monty Hall (M1), pero cuando ocurre este 'error' del presentador, el modelo Alternativo (M2) no se sorprende tanto, es decir que,  la evidencia baja, pero no colapsa. El modelo "sabe" que existe una pequeña posibilidad ($1-p$) de que el host sea torpe, por lo que asigna una probabilidad baja pero no cero.
+
+# ## Histograma de evidencias por modelo (plots de barras)
+#
+# Para el modelo base (M0) se observa una unica barra centrada en 0,055. Lo cual es algo que ya sabiamos solamente viendo el gráfico previo. Con respecto al Monty Hall, sucede algo mas que interesante. Cuando se siguen las reglas del juego el modelo M1 tiene una gran capacidad predictiva, sin embargo, el histograma muestra como hay bastantes situaciones (tantas como veces el presentador se olvide de las reglas del juego) donde la evidencia calculada es cero, esto ocurre exactamente 187 veces, de los 2000 episodios.
+#
+# Finalmente para el modelo alternativo, este gráfico quizas confunde un poco los rendimientos y si lo vemos rapidamente podemos llegar a pensar que quizas el MontyHall de alguna forma predice mejor que el Alternativo. Esto no es así! Si nos fijamos en el eje Y y vemos las escalas, podemos entender que el modelo Alternativo funciona mejor que el MontyHall. Sin embargo, esto no es así principalmente porque el modelo Alternativo nunca se sorprende demasiado. Es decir que no tiene ninguna combinación de (c, s, r) a la cual le asigne probabilidad 0. Esto en el grafico parece que es así pero en realidad son predicciones con muy baja probabilidad y no exactamente 0. Esto sucede cuando el presentador se olvida de las reglas en los primeros episodioos, luego el modelo aprende que puede pasar eso y la probabilidad nunca es 0.
